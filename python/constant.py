c16e70128aa071af04bcae70ab020f0215d7aec6
tango <ngotbtran@gmail.com>
2024-12-23 14:45:12 -0500

    start the project

---
+++ python/constant.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Constants
+"""
+
+class Pack_Type:
+	ORG = 0
+	RW = 1
+	CW = 2
+	DIAG = 3 
---
+++ python/helper.py
@@ -0,0 +1,159 @@
+import numpy as numpy
+
+def next_power2(x):
+    return 2 ** math.ceil(math.log2(x))
+
+
+class VectorHelper: 
+
+    @staticmethod
+    def _pack_row_wise(v, block_size, num_slots):
+        """
+        Clone a vector v to fill num_slots
+        1 -> 1111 2222 3333
+        2
+        3
+        """
+        n = len(v)
+        if (num_slots < n):
+            sys.exit("ERROR ::: [row_wise_vector] vector is longer than total slots")
+        if (num_slots == n):
+            if num_slots // block_size > 1: 
+                sys.exit("ERROR ::: [row_wise_vector] vector is too longer, can't duplicate")
+            return v
+
+        # print info
+        assert (num_slots % block_size == 0)
+        total_blocks = num_slots // block_size
+        free_slots = num_slots - n * block_size
+
+        # compute padding
+        packed = np.zeros(num_slots)
+        k = 0
+        for i in range (n):
+            for j in range(block_size):
+                packed[k] = v[i]
+                k +=1       
+    return packed
+
+
+    @staticmethod
+    def _pack_col_wise (v, block_size, num_slots):
+        """
+        Clone a vector v to fill num_slots
+        1 -> 1230 1230 1230
+        2
+        3
+        """
+        n = len(v)
+        if (block_size < n):
+            sys.exit(f"ERROR ::: [col_wise_vector] vector ({n}) is longer than size of a slot ({block_size})")
+        if (num_slots < n):
+            sys.exit(f"ERROR ::: [col_wise_vector] vector is longer than total slots")
+        if (num_slots == n):
+            return v
+
+        packed = np.zeros(num_slots)
+
+        # print info
+        assert (num_slots % block_size == 0)
+        total_blocks = num_slots // block_size 
+        free_slots = num_slots - n * total_blocks
+
+        k = 0 # index into vector to write
+        for i in range(total_blocks):
+            for j in range (n):
+                packed[k] = v[j]
+                k += 1
+            k += block_size - n
+     
+        return a
+
+
+
+class MatrixHelper: 
+
+
+    @staticmethod
+    def _pack_row_wise(matrix, block_size, num_slots):
+        """
+        Packing Matric M using row-wise
+        [[1 2 3] -> [1 2 3 0 4 5 6 0 7 8 9 0]
+        [4 5 6]
+        [7 8 9]]
+        """
+        assert num_slots % block_size == 0
+        n = len(matrix)  
+        m = len(matrix[0]) 
+        total_blocks = num_slots // block_size
+        # freeslots w.r.t block_size (not all free slots)
+        free_slots = num_slots - n * block_size
+        print(
+            "#\t [enc. matrix] n = %d, m = %d, #slots = %d, bs = %d, blks = %d, #freeslots = %d, used <= %.3f"
+            % (
+                n,
+                m,
+                num_slots,
+                block_size,
+                total_blocks,
+                free_slots,
+                (num_slots - free_slots) / num_slots,
+            )
+        )
+
+        if num_slots < n * m:
+            Exception("encrypt_matrix ::: Matrix is too big compared with num_slots")
+
+        packed = np.zeros(num_slots)
+        k = 0  # index into vector to write
+        for i in range(n):
+            for j in range(m):
+                packed[k] = matrix[i][j]
+                k += 1
+            for j in range(m, block_size):
+                packed[k] = 0
+                k += 1
+        return packed
+
+
+    @staticmethod
+    def _pack_col_wise(matrix, block_size, num_slots, verbose = 0):
+        """
+        Packing Matric M using row-wise
+        [[1 2 3] -> [1 4 7 0 2 5 8 0 3 6 9 0]
+         [4 5 6]
+         [7 8 9]]
+        """
+        assert num_slots % block_size == 0
+        cols = len(matrix) 
+        rows = len(matrix[0])  
+        total_blocks = num_slots // block_size
+        free_slots = num_slots - cols * block_size
+
+        if (verbose):
+            print(
+                "#\t [enc. matrix] n = %d, m = %d, #slots = %d, bs = %d, blks = %d, #freeslots = %d, used <= %.3f"
+                % (
+                    cols,
+                    rows,
+                    num_slots,
+                    block_size,
+                    total_blocks,
+                    free_slots,
+                    (num_slots - free_slots) / num_slots,
+                )
+            )
+
+        if num_slots < cols * rows:
+            Exception("encrypt_matrix ::: Matrix is too big compared with num_slots")
+
+        packed = np.zeros(num_slots)
+        k = 0  # index into vector to write
+
+        for col in range (cols):
+                for row in range (block_size):
+                    if row < rows: 
+                        packed[k] = matrix[row][col]
+                    k = k + 1
+
+        return packed
